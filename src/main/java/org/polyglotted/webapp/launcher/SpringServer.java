76f40c42c0052979e3f1c7b702c76d1fab24e94c
Shankar Vasudevan <shankar@vshank77.com>
2013-08-29 00:26:30 +0100

    changed impl for SpringApp main

--- src/main/java/org/polyglotted/webapp/launcher/SpringServer.java
+++ src/main/java/org/polyglotted/webapp/launcher/SpringServer.java
@@ -1,16 +1,28 @@
 package org.polyglotted.webapp.launcher;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class SpringServer implements Server {
+public class SpringServer implements Server, Runnable {
 
     private static final String APP_XML = "/META-INF/spring/main-context.xml";
 
     private AbstractApplicationContext applicationContext;
+    private CountDownLatch latch = new CountDownLatch(1);
 
     @Override
     public void start() throws Exception {
+        final Thread runnerThread = new Thread(this);
+        runnerThread.setDaemon(true);
+        runnerThread.setName("main.runner.thread");
+        runnerThread.start();
+        latch.await();
+    }
+
+    @Override
+    public void run() {
         applicationContext = new ClassPathXmlApplicationContext(APP_XML);
         applicationContext.start();
     }
@@ -18,5 +30,6 @@ public class SpringServer implements Server {
     @Override
     public void stop() throws Exception {
         applicationContext.close();
+        latch.countDown();
     }
 }
